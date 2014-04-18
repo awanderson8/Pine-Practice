e3c23ec163624ea9a1cc88f8151e6f881de9093c
awanderson8 <awanderson8@gmail.com>
2014-04-18 14:11:40 -0500

    Age in seconds

---
+++ Numbers1.rb
@@ -0,0 +1,17 @@
+puts "How many hours are in a year?"
+puts 24 * 365 
+puts ""
+
+puts "How many minutes are in a decade?"
+puts 60 * 24 * 365 * 10
+
+puts "Let me tell you how old you are in seconds"
+puts "What's your age in years?"
+age = gets.chomp
+
+age =age.to_i
+
+age_seconds = age * 365 * 24 * 60
+
+puts ""
+puts "It looks like you are #{age_seconds} seconds old"
