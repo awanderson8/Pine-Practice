a3907d817351b1e4fc203bd9014d02b7f5cc59e2
awanderson8 <awanderson8@gmail.com>
2014-04-23 21:42:06 -0500

    bottles of beer and table of contents

---
+++ bottles.rb
@@ -0,0 +1,8 @@
+bottles = 10
+while bottles > 0
+
+	puts "#{bottles} bottles of beer on the wal, #{bottles} bottles of beer"
+		bottles = (bottles - 1)
+	puts "take one down pass it around, #{bottles} bottles of beer on the wall!"
+	puts ""
+end
